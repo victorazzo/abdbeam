378d4324e26a3860b2a9e1b139f2372fe76d2a1e
victorazzo <victorazzo@gmail.com>
2019-02-10 20:01:05 -0500

    Update documentation version to 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Danilo Seixas Victorazzo'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
