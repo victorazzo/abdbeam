ea363991bdb8f4d370f0828e4037bfaaed4b4aa7
victorazzo <victorazzo@gmail.com>
2019-11-28 09:05:26 -0800

    Include plot_section_results

--- abdbeam/__init__.py
+++ abdbeam/__init__.py
@@ -1,5 +1,5 @@
 from .core import Section, Point, Segment, Load
 from .materials import (Material, Isotropic, ShearConnector, PlyMaterial,
                         Laminate)
-from .plots import plot_section, plot_section_loads
+from .plots import plot_section, plot_section_results, plot_section_loads
 
